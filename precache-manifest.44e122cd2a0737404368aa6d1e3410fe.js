0c84f2b9dc373e4833f788e73375c5f1e62d4525
Radosław Kuczyński <krooopel@gmail.com>
2020-01-23 20:16:10 +0100

    Updates

---
+++ precache-manifest.44e122cd2a0737404368aa6d1e3410fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d6bf9c08d2dfc33890d780136ee1103",
+    "url": "/react-first-semestr-project/index.html"
+  },
+  {
+    "revision": "62e78fdc50a9977c2d7e",
+    "url": "/react-first-semestr-project/static/css/main.7984b983.chunk.css"
+  },
+  {
+    "revision": "30b4905a131229218a6f",
+    "url": "/react-first-semestr-project/static/js/2.64d8b4fb.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-first-semestr-project/static/js/2.64d8b4fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "62e78fdc50a9977c2d7e",
+    "url": "/react-first-semestr-project/static/js/main.72b1a32f.chunk.js"
+  },
+  {
+    "revision": "132ca8079ad16b5c81a9",
+    "url": "/react-first-semestr-project/static/js/runtime-main.257e8481.js"
+  }
+]);
